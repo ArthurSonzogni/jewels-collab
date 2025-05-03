3833ce577933e76d6c70022d3940a2d3acf0bf54
ArthurSonzogni <sonzogniarthur@gmail.com>
2025-05-03 19:13:35 +0200

    add collections.

---
+++ pages/collection/[slug].vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container">
+
+    <h1 class="title font-title text-center">
+      {{ collection.title }}
+    </h1>
+
+    <p class="description text-center">
+      {{ collection.description }}
+    </p>
+
+    <div class="container">
+      <h1 class="other-product title font-title">Dans la collection</h1>
+      <div class="other-products-list">
+        <div
+          v-for="(product, index) in allProducts"
+          :key="index"
+          class="other-product"
+          >
+          <NuxtLink :to="product.path"> 
+          <img class="miniature"
+               v-if="product.meta.images[0]"
+               :key="index"
+               :src="`${config.app.baseURL}${product.meta.images[0].image}`"
+               />
+          <h2>{{ product.title }}</h2>
+          <p class="price">{{ product.meta.price }}</p>
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const config = useRuntimeConfig();
+
+const slug = useRoute().params.slug;
+
+// Fetch the collection data.
+const { data: collection } = await useAsyncData(() =>
+  queryCollection('collection').path(`/collection/${slug}`).first()
+);
+
+// Fetch all the products, but filter the one that are in the collection.
+const { data: allProducts } = await useAsyncData(() =>
+  queryCollection('product').all()
+);
+
+// Filter out the products that are not in the collection.
+allProducts.value = allProducts.value.filter((product) => {
+  return product.meta.collection === slug;
+});
+
+
+</script>
+
+
+<style scoped lang="scss">
+.other-product {
+  padding: 20px 0;
+}
+
+.other-products-list {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.other-product {
+  --width: 300px;
+  max-width: var(--width);
+  margin: 20px;
+}
+
+.miniature {
+  width: var(--width);
+  height: var(--width);
+  object-fit: cover;
+}
+</style>
